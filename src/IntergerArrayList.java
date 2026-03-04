1f70f85109aaa1fd1b696a51c5a5e8e969fe3260
arpit375 <arpit.srivastava375@gmail.com>
2026-03-04 02:24:06 -0500

    program for sum of 2 numbers

---
+++ src/IntergerArrayList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.*;
+/*
+public class IntergerArrayList {
+    public static void main(String args[])
+    {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(4);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(7);
+        System.out.println(numbers);
+        Collections.sort(numbers);
+        System.out.println("the sorted list is");
+        System.out.println(numbers);
+    }
+*/
---
+++ src/SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args)
+    {
+        int a,b, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 numbers");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sum = a + b;
+        System.out.println("Sum of "+a+" and "+b+" is "+sum);
+    }
+}
